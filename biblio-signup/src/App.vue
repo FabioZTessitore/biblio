cf40657a4582194075904e821f9f44c6b4bde03c
Fabio Z Tessitore <fabioztessitore@gmail.com>
2025-09-19 12:38:11 +0200

    initial style

--- biblio-signup/src/App.vue
+++ biblio-signup/src/App.vue
@@ -4,9 +4,9 @@ import Signup from "./Signup.vue";
 
 <template>
   <header class="header">
-    <h1 class="header-primary">Biblio App</h1>
-    <h2 class="header-secondary">Signup</h2>
-    <h3 class="header-tertiary">Only admin</h3>
+    <h1 class="heading-primary">Biblio App</h1>
+    <h2 class="heading-secondary">Signup</h2>
+    <h3 class="heading-tertiary">Only admin</h3>
   </header>
 
   <main class="main"><Signup /></main>
