e2af7a959827e57260c7771697389e4d2aab4885
Fabio Z Tessitore <fabioztessitore@gmail.com>
2025-09-20 11:21:36 +0200

    primo componente test

---
+++ biblio/app/signup.tsx
@@ -0,0 +1,13 @@
+import { Text, StyleSheet } from "react-native";
+
+const Signup = () => {
+  return <Text style={styles.signup}>Signup Component</Text>;
+};
+
+const styles = StyleSheet.create({
+  signup: {
+    color: "red",
+  },
+});
+
+export default Signup;
