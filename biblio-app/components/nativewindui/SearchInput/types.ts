bae3375471afe903636768798a9d3ccb6b304f04
tonyiovino <antonioiovino2004tony@gmail.com>
2025-11-14 02:32:51 +0100

    SearchInput Component

---
+++ biblio-app/components/nativewindui/SearchInput/types.ts
@@ -0,0 +1,10 @@
+import type { TextInput, TextInputProps } from 'react-native';
+
+export type SearchInputProps = TextInputProps & {
+  containerClassName?: string;
+  iconContainerClassName?: string;
+  cancelText?: string;
+  iconColor?: string;
+};
+
+export type SearchInputRef = TextInput;
