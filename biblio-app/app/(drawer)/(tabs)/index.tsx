0b59cdf3ec4f23e1dfa9de294bfea787d04b23ed
tonyiovino <antonioiovino2004tony@gmail.com>
2025-11-19 01:28:56 +0100

    BookCard component

--- biblio-app/app/(drawer)/(tabs)/index.tsx
+++ biblio-app/app/(drawer)/(tabs)/index.tsx
@@ -1,78 +1,21 @@
 import { View, FlatList } from 'react-native';
+import { BookCard } from '~/components/partials/BookCard';
 import { Button } from '~/components/nativewindui/Button';
 import { Text } from '~/components/Text';
 import { useBookStore, useUserStore } from '~/store';
 import { Icon } from '~/components/Icon';
-import { Book } from '~/store/book';
-import { FiltersSheetModal } from '~/components/FiltersSheetModal';
-// import { LinearGradient } from 'expo-linear-gradient';
 
-const ListItem = ({
-  item,
-  selected,
-  onPress,
-}: {
-  item: Book;
-  selected?: boolean;
-  onPress: () => void;
-}) => (
-  <View className={'flex-1 rounded-2xl bg-card p-4'}>
-    <View className="justify-center gap-6 rounded-lg">
-      {/* Immagine e Valutazione */}
-      <View className="gap-4">
-        <View className="h-32 rounded-2xl bg-blue-900"></View>
-
-        <View className="flex-row items-center gap-1">
-          <Icon name="star" size={'body'} color="#ca8a04" />
-          <Text variant="label">4,6</Text>
-          {/*TODO: fix colors tailwind css 'muted'*/}
-          <Text variant="label" className="color-gray-400">
-            {'(+150 reviews)'}
-          </Text>
-        </View>
-      </View>
-
-      {/* Titolo e Autore */}
-      <View className="gap-2">
-        <Text variant="body" className="text-wrap">
-          {item.title} Lorem ipsum dolor sit amet dwdwdwd fefwewe fewfwef fewfwef
-        </Text>
-        <Text variant="label" className="text-wrap text-gray-600">
-          di {item.author}
-        </Text>
-      </View>
-
-      {/* Disponibilità e CTA*/}
-      <View className="flex-row items-center justify-between">
-        <View className="flex-row items-center gap-2">
-          <Icon size={'label'} name="circle" color="#4ade80"></Icon>
-          <Text
-            weight={'light'}
-            style={{ includeFontPadding: false }}
-            className="flex-shrink uppercase">
-            Disponibile
-          </Text>
-        </View>
-        <Button
-          android_ripple={{ foreground: true, color: '#fed7aa' }}
-          onPress={onPress}
-          className="bg-primary-test bg-orange-600">
-          <Text>{selected ? 'Rimuovi' : '+ Libreria'}</Text>
-        </Button>
-      </View>
-    </View>
-  </View>
-);
 
 export default function Index() {
-  const { books } = useBookStore();
-  const { library } = useUserStore();
+  const { library, addBookToLibrary } = useUserStore();
 
   const handlePress = (item: Book) => {
     console.log('Pressed:', item.title);
+    if (library.includes(item)) return; // Error: Libro già aggiunto alla libreria
+    addBookToLibrary(item);
   };
 
-  // const isSelected = (id: string) => library.forEach((book) => book.id === id);
+  const isSelected = (id: string) => library.some((book) => book.id === id);
 
   return (
     <View className="flex-1 gap-4 px-4">
@@ -93,9 +36,9 @@ export default function Index() {
           className="rounded-md"
           showsVerticalScrollIndicator={false}
           renderItem={({ item }) => (
-            <ListItem
+            <BookCard
               item={item}
-              // selected={isSelected(item.id)}
+              selected={isSelected(item.id)}
               onPress={() => handlePress(item)}
             />
           )}
---
+++ biblio-app/components/partials/BookCard.tsx
@@ -0,0 +1,84 @@
+import { View, Image } from 'react-native';
+import { Text } from '~/components/Text';
+import { Book } from '~/store/book';
+import { Icon } from '~/components/Icon';
+import { cn } from '~/lib/cn';
+import { Button } from '~/components/nativewindui/Button';
+import { useColorScheme } from '~/lib/useColorScheme';
+
+interface BookCardProps {
+  item: Book;
+  selected?: boolean;
+  onPress: () => void;
+}
+
+const BookCard = ({ item, selected, onPress }: BookCardProps) => {
+  const { colors } = useColorScheme();
+
+  return (
+    <View className={'flex-1 rounded-2xl bg-card p-4'}>
+      <View className="justify-center gap-6 rounded-lg">
+        {/* Immagine e Valutazione */}
+        <View className="gap-4">
+          <Image
+            className="h-32 rounded-2xl"
+            resizeMethod="resize"
+            resizeMode="cover"
+            source={{
+              uri: 'https://images.unsplash.com/photo-1592496431122-2349e0fbc666?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Ym9vayUyMGNvdmVyfGVufDB8fDB8fHww',
+            }}></Image>
+
+          <View className="flex-row items-center gap-1">
+            <Icon name="star" size={'body'} color="#ca8a04" />
+            <Text variant="label">4,6</Text>
+            {/*TODO: fix colors tailwind css 'muted'*/}
+            <Text variant="label" className="color-gray-400">
+              {'(+150 reviews)'}
+            </Text>
+          </View>
+        </View>
+
+        {/* Titolo e Autore */}
+        <View className="gap-2">
+          <Text variant="body" className="text-wrap">
+            {item.title}
+          </Text>
+          <Text variant="label" className="text-wrap text-gray-400">
+            di {item.author}
+          </Text>
+        </View>
+
+        {/* Disponibilità e CTA*/}
+        <View className="flex-row items-center justify-between">
+          <View className="flex-row items-center gap-2">
+            <Icon
+              size={'label'}
+              name="circle"
+              color={item.availbale ? '#4ade80' : '#BC2F2F'}></Icon>
+            <Text
+              weight={'light'}
+              style={{ includeFontPadding: false }}
+              className="flex-shrink uppercase">
+              {item.availbale ? 'Disponibile' : 'Non Disponibile'}
+            </Text>
+          </View>
+          {selected ? (
+            <Button className="bg-transparent">
+              <Text className="text-success">{'Aggiunto'}</Text>
+            </Button>
+          ) : (
+            <Button
+              android_ripple={{ foreground: true, color: '#fed7aa' }}
+              onPress={onPress}
+              className={'bg-primary-test bg-orange-600'}>
+              <Icon size={'body'} name="add" />
+              <Text>{'Libreria'}</Text>
+            </Button>
+          )}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export { BookCard };
