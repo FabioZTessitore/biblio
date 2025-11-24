1480930100c1e4b240844b4270cce08a8acf42b6
tonyiovino <antonioiovino2004tony@gmail.com>
2025-11-24 02:13:16 +0100

    login header

--- biblio-app/app/(drawer)/_layout.tsx
+++ biblio-app/app/(drawer)/_layout.tsx
@@ -1,10 +1,22 @@
 import { Drawer } from 'expo-router/drawer';
+import { useColorScheme } from '~/lib/useColorScheme';
 
 export default function TabLayout() {
+  const { colors } = useColorScheme();
+
   return (
     <Drawer screenOptions={{ headerShown: false }}>
       <Drawer.Screen name="(tabs)" options={{ title: 'Home' }} />
-      <Drawer.Screen name="login" options={{ title: 'Login' }} />
+      <Drawer.Screen
+        name="login"
+        options={{
+          title: 'Login',
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: colors.background,
+          },
+        }}
+      />
     </Drawer>
   );
 }
