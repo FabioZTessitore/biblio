ccbb7fa60d556f17f4757fe5921033f481d9b620
tonyiovino <antonioiovino2004tony@gmail.com>
2026-01-04 15:57:09 +0100

    empty state handling in book list

--- biblio-app/app/(drawer)/(tabs)/index.tsx
+++ biblio-app/app/(drawer)/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { View, FlatList, RefreshControl, Pressable } from 'react-native';
 import { useFiltersStore, useLibraryStore, useBiblioStore, useUserStore } from '~/store';
 import { FiltersSheetModal, BookSheetModal } from '~/components';
 import { LinearGradient } from 'expo-linear-gradient';
-import { BookCard } from '~/components/partials';
+import { BookCard, EmptyState } from '~/components/partials';
 import { Button } from '~/components/nativewindui/Button';
 import { Icon, Text } from '~/components/ui';
 import { useColorScheme } from '~/lib/useColorScheme';
@@ -61,94 +61,85 @@ export default function Index() {
   const filteredBooks = applyFilters(books, filters);
 
   return (
-    <View className="flex-1 gap-4 px-4">
-      {/* <View className="rounded-2xl bg-card">
-        <SearchInput textContentType="none" autoComplete="off" />
-      </View>
-      <View className="flex-row">
-        <View className="rounded-full bg-secondary px-2 py-1">
-          <Text variant={'label'}>Aggiungin +</Text>
-        </View>
-      </View> */}
-
-      <View className="flex-1">
-        <FlatList
-          ListEmptyComponent={() => {
-            if (books.length === 0) {
-              return <Text className="text-center">Non ci sono libri nella tua libreria.</Text>;
-            }
-            if (filteredBooks.length === 0) {
-              return (
-                <>
-                  <Text className="text-center">
-                    Nessun libro corrisponde ai filtri selezionati.
-                  </Text>
-                  <Pressable onPress={resetFilters}>
-                    <Text className="text-center" color="primary">
-                      Elimina
-                    </Text>
-                  </Pressable>
-                </>
-              );
-            }
-            return null;
-          }}
-          data={filteredBooks}
-          keyExtractor={(item) => item.id}
-          contentContainerClassName="gap-14 py-8"
-          className="rounded-md"
-          showsVerticalScrollIndicator={false}
-          initialNumToRender={10}
-          maxToRenderPerBatch={10}
-          renderItem={({ item }) => (
-            <BookCard item={item} selected={isSelected(item.id)} onPress={handlePressMemo(item)} />
-          )}
-          refreshControl={
-            <RefreshControl
-              colors={[colors.primary]}
-              tintColor={colors.primary}
-              progressBackgroundColor={colors.card}
-              refreshing={isLoading}
-              enabled={false}
-            />
+    <View className="flex-1 px-4">
+      <FlatList
+        ListEmptyComponent={() => {
+          if (books.length === 0) {
+            return <Text className="text-center">Non ci sono libri nella tua scuola.</Text>;
           }
-        />
-
-        {membership.role === 'staff' && (
-          <View className="absolute bottom-0 right-2 z-10 h-20">
-            <Button className="rounded-2xl p-4" size={'none'} onPress={() => setBookModal(true)}>
-              <Icon name="add" />
-            </Button>
-          </View>
+          if (filteredBooks.length === 0) {
+            return (
+              <View className="flex-1 justify-center">
+                <EmptyState
+                  icon={'filter-off-outline'}
+                  title={'Nessun libro corrisponde ai filtri selezionati.'}
+                  subtitle=""
+                />
+                <Pressable onPress={resetFilters}>
+                  <Text className="text-center" color="primary">
+                    Ripristina
+                  </Text>
+                </Pressable>
+              </View>
+            );
+          }
+          return null;
+        }}
+        data={filteredBooks}
+        keyExtractor={(item) => item.id}
+        contentContainerClassName="gap-14 py-8"
+        className="rounded-md"
+        showsVerticalScrollIndicator={false}
+        initialNumToRender={10}
+        maxToRenderPerBatch={10}
+        renderItem={({ item }) => (
+          <BookCard item={item} selected={isSelected(item.id)} onPress={handlePressMemo(item)} />
         )}
+        refreshControl={
+          <RefreshControl
+            colors={[colors.primary]}
+            tintColor={colors.primary}
+            progressBackgroundColor={colors.card}
+            refreshing={isLoading}
+            enabled={false}
+          />
+        }
+      />
 
-        <LinearGradient
-          colors={[colors.background, convertToRGBA(colors.background, 0)]}
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            height: 10,
-            zIndex: 10,
-          }}
-          pointerEvents="none"
-        />
+      {membership.role === 'staff' && (
+        <View className="absolute bottom-0 right-2 z-10 h-20">
+          <Button className="rounded-2xl p-4" size={'none'} onPress={() => setBookModal(true)}>
+            <Icon name="add" />
+          </Button>
+        </View>
+      )}
 
-        <FiltersSheetModal />
-        {membership.role === 'staff' && (
-          <>
-            <BookSheetModal mode="add" visible={bookModal} onClose={() => setBookModal(false)} />
+      <LinearGradient
+        colors={[colors.background, convertToRGBA(colors.background, 0)]}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 10,
+          zIndex: 10,
+        }}
+        pointerEvents="none"
+      />
 
-            <BookSheetModal
-              mode="edit"
-              visible={bookEditModal}
-              bookId={bookIdToEdit}
-              onClose={() => setBookEditModal(false)}
-            />
-          </>
-        )}
-      </View>
+      <FiltersSheetModal />
+      {membership.role === 'staff' && (
+        <>
+          <BookSheetModal mode="add" visible={bookModal} onClose={() => setBookModal(false)} />
+
+          <BookSheetModal
+            mode="edit"
+            visible={bookEditModal}
+            bookId={bookIdToEdit}
+            onClose={() => setBookEditModal(false)}
+          />
+        </>
+      )}
     </View>
   );
 }
